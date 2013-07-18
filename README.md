65ad9898f649aa29deadd88b070bd31c20f66bf3
Teddy Chan <teddychan@gmail.com>
2013-07-18 09:30:17 +0800

    first release

---
+++ README.md
@@ -0,0 +1,35 @@
+## What is node-phone?
+`node-phone` is used to normalize the phone number into a E.164 format.
+
+The common problem is user normally input the phone number in this way:
+`(817) 569-8900` or
+`817569-8900` or
+`1(817) 569-8900` or
+`+1(817) 569-8900` or ...
+
+What we want is always: `+18175698900`
+
+##FAQ
+1. Does node-phone do any phone validation?
+Ans: Yes or no
+
+
+## Example
+```
+npm install phone
+
+var phone = require('phone`);
+
+phone('(817) 569-8900'); // return +18175698900
+phone('(817) 569-8900', 'USA'); // return +18175698900
+phone('(817) 569-8900', 'HKG'); // return null
+phone('+1(817) 569-8900', HKG); // return +18175698900
+phone('(817) 569-8900', ''); // return +18175698900
+phone('6123-6123', ''); // return null, as default country is USA
+phone('6123-6123', 'HKG'); // return +85261236123
+
+```
+
+The latest version is using:
+
+http://www.itu.int/dms_pub/itu-t/opb/sp/T-SP-E.164D-11-2011-PDF-E.pdf
---
+++ test/index.js
@@ -0,0 +1,99 @@
+var should = require('should'),
+	phone = require('../lib/index');
+
+describe('Testing USA Phone', function() {
+
+	describe("phone('+1 (817) 569-8900', '')", function() {
+		it('returns +18175698900', function() {
+			var result = phone('+1 (817) 569-8900', '');
+			result.should.eql('+18175698900');
+		});
+	});
+
+	describe("phone('212345678', '')", function() {
+		it('returns null', function() {
+			var result = (phone('212345678', '') === null) ? true : false;
+			result.should.eql(true);
+		});
+	});
+
+
+	describe("phone('22-6569-8900', '')", function() {
+		it('returns +', function() {
+			var result = phone('22-6569-8900', '');
+			result.should.eql('+12265698900');
+		});
+	});
+
+	describe("phone('+1 (817) 569-8900', 'uniTed States')", function() {
+		it('returns +18175698900', function() {
+			var result = phone('+1 (817) 569-8900', 'uniTed States');
+			result.should.eql('+18175698900');
+		});
+	});
+
+	describe("phone('+1 (817) 569-8900', ' uniTed States ')", function() {
+		it('returns +18175698900', function() {
+			var result = phone('+1 (817) 569-8900', ' uniTed States ');
+			result.should.eql('+18175698900');
+		});
+	});
+
+	describe("phone('+1 (817) 569-8900', 'USA')", function() {
+		it('returns +18175698900', function() {
+			var result = phone('+1 (817) 569-8900', 'USA');
+			result.should.eql('+18175698900');
+		});
+	});
+
+	describe("phone('+1 (817) 569-8900', 'US')", function() {
+		it('returns +18175698900', function() {
+			var result = phone('+1 (817) 569-8900', 'US');
+			result.should.eql('+18175698900');
+		});
+	});
+
+});
+
+
+
+describe('Testing HK Phone', function() {
+
+	describe("phone('6569-8900', '')", function() {
+		it('returns null', function() {
+			var result = (phone('6569-8900', '') === null) ? true : false;
+			result.should.eql(true);
+		});
+	});
+
+	describe("phone('+852 8-569-8900', 'HKG')", function() {
+		it('returns +85285698900', function() {
+			var result = phone('+852 8-569-8900', 'HKG');
+			result.should.eql('+85285698900');
+		});
+	});
+
+	describe("phone('+852 8-569-8900', 'USA')", function() {
+		it('returns +85285698900', function() {
+			var result = phone('+852 8-569-8900', 'USA');
+			result.should.eql('+85285698900');
+		});
+	});
+
+	describe("phone('+852 8-569-8900', 'HK')", function() {
+		it('returns +85285698900', function() {
+			var result = phone('+852 8-569-8900', 'HK');
+			result.should.eql('+85285698900');
+		});
+	});
+
+	describe("phone('212345678', 'HKG')", function() {
+		it('returns null', function() {
+			var result = (phone('212345678', 'HKG') === null) ? true : false;
+			result.should.eql(true);
+		});
+	});
+
+});
+
+
