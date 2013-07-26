2cc602bc416975ba8db027b0c1d9bf16cf42519a
Teddy Chan <teddychan@gmail.com>
2013-07-26 15:40:05 +0800

    added UK test case

--- test/index.js
+++ test/index.js
@@ -110,3 +110,46 @@ describe('Testing HK Phone', function() {
 });
 
 
+
+
+
+describe('Testing UK Phone', function() {
+
+	// treat it as USA first
+	// then remove leading 0s for all countries except 'GAB', 'CIV', 'COG'
+	// if 10 digit, good. it is USA, add "+1" as prefix
+
+	describe("phone('01902687632', '')", function() {
+		it('returns +11902687632', function() {
+			var result = phone('01902687632', '');
+			result.should.eql('+11902687632');
+		});
+	});
+
+	describe("phone('01902687632', null)", function() {
+		it('returns +11902687632', function() {
+			var result = phone('01902687632', null) ;
+			result.should.eql('+11902687632');
+		});
+	});
+
+
+	// remove leading 0s for all countries except 'GAB', 'CIV', 'COG'
+	// if 8 digit, good. it is HKG, add "+852" as prefix, else , return null
+
+	describe("phone('01902687632', 'HKG')", function() {
+		it('returns +8521902687632', function() {
+			var result = (phone('01902687632', 'HKG') === null);
+			result.should.eql(true);
+		});
+	});
+
+	describe("phone('01902687632', 'GBR')", function() {
+		it('returns +441902687632', function() {
+			var result = (phone('01902687632', 'GBR') === null);
+			result.should.eql(true);
+		});
+	});
+
+
+});
