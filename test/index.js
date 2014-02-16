88d38942b9d1bcd13d4b7f99c4f8f7d100b47857
Teddy Chan <teddychan@gmail.com>
2014-02-16 22:21:49 +0800

    done CAN test case

--- test/index.js
+++ test/index.js
@@ -6,17 +6,32 @@ var test_number, test_country, test_result, test_explain;
 
 /* suggest 10 test case for each country, except USA
 
- without +, null --> invalid
- without +, '' --> invalid
- without +, valid iso3  --> valid
- without +, valid begin with, invalid iso3 --> invalid
- without +, invalid begin with, valid iso3 --> invalid
+ Test Case, for non-USA
 
- with +, null
- with +, ''
- with +, valid iso3
- with +, valid begin with, invalid iso3
- with +, invalid begin with, valid iso3 --> invalid
+ valid +phone, null
+ valid +phone, valid iso
+ valid +phone, invalid iso
+ valid +phone, valid name
+ valid +phone, invalid name
+
+ invalid +phone, null
+ invalid +phone, valid iso
+ invalid +phone, invalid iso
+ invalid +phone, valid name
+ invalid +phone, invalid name
+
+ valid phone, null
+ valid phone, valid iso
+ valid phone, invalid iso
+ valid phone, valid name
+ valid phone, invalid name
+
+ invalid phone, null
+ invalid phone, valid iso
+ invalid phone, invalid iso
+ invalid phone, valid name
+ invalid phone, invalid name
+
  */
 
 describe('Testing USA Phone', function() {
@@ -34,7 +49,7 @@ describe('Testing USA Phone', function() {
 	test_number = '+1 (817) 569-8900';
 	test_country = '';
 	test_result = ['+18175698900', 'USA'];
-	test_explain = 'returns returns [' + test_result + ']';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -45,7 +60,7 @@ describe('Testing USA Phone', function() {
 	test_number = '+1 (817) 569-8900';
 	test_country = null;
 	test_result = ['+18175698900', 'USA'];
-	test_explain = 'returns returns [' + test_result + ']';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -56,7 +71,7 @@ describe('Testing USA Phone', function() {
 	test_number = '2121234567';
 	test_country = '';
 	test_result = ['+12121234567', 'USA'];
-	test_explain = 'returns returns [' + test_result + '], no country given, make USA, fit USA length without country code, auto append. valid zip code: 212';
+	test_explain = 'returns [' + test_result + '], no country given, make USA, fit USA length without country code, auto append. valid zip code: 212';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -68,7 +83,7 @@ describe('Testing USA Phone', function() {
 	test_number = '22-6569-8900';
 	test_country = '';
 	test_result = [];
-	test_explain = 'returns returns [' + test_result + '], as 226 is NOT a valid USA code';
+	test_explain = 'returns [' + test_result + '], as 226 is NOT a valid USA code';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -80,7 +95,7 @@ describe('Testing USA Phone', function() {
 	test_number = '22-5569-8900';
 	test_country = '';
 	test_result = ['+12255698900', 'USA'];
-	test_explain = 'returns returns [' + test_result + '], as 225 is a valid USA code';
+	test_explain = 'returns [' + test_result + '], as 225 is a valid USA code';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -92,7 +107,7 @@ describe('Testing USA Phone', function() {
 	test_number = '+1 (817) 569-8900';
 	test_country = 'United States';
 	test_result = ['+18175698900', 'USA'];
-	test_explain = 'returns returns [' + test_result + ']';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -104,7 +119,7 @@ describe('Testing USA Phone', function() {
 	test_number = '+1 (817) 569-8900';
 	test_country = 'United States ';
 	test_result = ['+18175698900', 'USA'];
-	test_explain = 'returns returns [' + test_result + ']';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -115,7 +130,7 @@ describe('Testing USA Phone', function() {
 	test_number = '+1 (817) 569-8900';
 	test_country = 'USA';
 	test_result = ['+18175698900', 'USA'];
-	test_explain = 'returns returns [' + test_result + ']';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -127,7 +142,7 @@ describe('Testing USA Phone', function() {
 	test_number = '+1 (817) 569-8900';
 	test_country = 'USA ';
 	test_result = ['+18175698900', 'USA'];
-	test_explain = 'returns returns [' + test_result + ']';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -139,7 +154,7 @@ describe('Testing USA Phone', function() {
 	test_number = '+1 (817) 569-8900';
 	test_country = 'US';
 	test_result = ['+18175698900', 'USA'];
-	test_explain = 'returns returns [' + test_result + ']';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -150,7 +165,7 @@ describe('Testing USA Phone', function() {
 	test_number = '+1 (817) 569-8900';
 	test_country = ' US ';
 	test_result = ['+18175698900', 'USA'];
-	test_explain = 'returns returns [' + test_result + ']';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -162,7 +177,7 @@ describe('Testing USA Phone', function() {
 	test_number = '+1 (817) 569-8900';
 	test_country = 'HKG';
 	test_result = [];
-	test_explain = 'returns returns [' + test_result + '], it is NOT a valid HKG phone number.';
+	test_explain = 'returns [' + test_result + '], it is NOT a valid HKG phone number.';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -176,9 +191,9 @@ describe('Testing USA Phone', function() {
 describe('Testing CAN Phone', function() {
 
 	test_number = '+1 (403) 569-8900';
-	test_country = '';
+	test_country = null;
 	test_result = ['+14035698900', 'CAN'];
-	test_explain = 'returns returns [' + test_result + ']';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -186,11 +201,109 @@ describe('Testing CAN Phone', function() {
 		});
 	});
 
+	test_number = '+1 (403) 569-8900';
+	test_country = 'CAN';
+	test_result = ['+14035698900', 'CAN'];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
 
 	test_number = '+1 (403) 569-8900';
+	test_country = 'USA';
+	test_result = [];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '+1 (403) 569-8900';
+	test_country = 'Canada';
+	test_result = ['+14035698900', 'CAN'];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '+1 (403) 569-8900';
+	test_country = 'Canadas';
+	test_result = [];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '+1 (401) 569-8900';
+	test_country = null;
+	test_result = [];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '+1 (401) 569-8900';
+	test_country = 'CAN';
+	test_result = [];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '+1 (401) 569-8900';
+	test_country = 'HKG';
+	test_result = [];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '+1 (401) 569-8900';
+	test_country = 'Canada';
+	test_result = [];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '+1 (401) 569-8900';
+	test_country = 'Canadas';
+	test_result = [];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '(403) 569-8900';
 	test_country = null;
 	test_result = ['+14035698900', 'CAN'];
-	test_explain = 'returns returns [' + test_result + ']';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -200,8 +313,8 @@ describe('Testing CAN Phone', function() {
 
 	test_number = '(403) 569-8900';
 	test_country = 'CAN';
-	test_result = [];
-	test_explain = 'returns returns [' + test_result + '], it is a USA zip code';
+	test_result = ['+14035698900', 'CAN'];
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -210,9 +323,9 @@ describe('Testing CAN Phone', function() {
 	});
 
 	test_number = '(403) 569-8900';
-	test_country = null;
+	test_country = 'USA';
 	test_result = [];
-	test_explain = 'returns returns [' + test_result + '], it is a USA zip code';
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
@@ -221,10 +334,65 @@ describe('Testing CAN Phone', function() {
 	});
 
 
-	test_number = '212345678';
-	test_country = null;
+	test_number = '(403) 569-8900';
+	test_country = 'Canada';
+	test_result = ['+14035698900', 'CAN'];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '(403) 569-8900';
+	test_country = 'United States';
 	test_result = [];
-	test_explain = 'returns returns [' + test_result + '], it is a USA zip code';
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '(401) 569-8900';
+	test_country = null;
+	test_result = ['+14015698900', 'USA'];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '(401) 569-8900';
+	test_country = 'HKG';
+	test_result = [];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '(401) 569-8900';
+	test_country = 'Canada';
+	test_result = [];
+	test_explain = 'returns [' + test_result + ']';
+	describe("phone('" + test_number + "', '" + test_country + "')", function() {
+		it(test_explain, function() {
+			var result = phone(test_number, test_country);
+			result.should.eql(test_result);
+		});
+	});
+
+	test_number = '(401) 569-8900';
+	test_country = 'Hong Kong';
+	test_result = [];
+	test_explain = 'returns [' + test_result + ']';
 	describe("phone('" + test_number + "', '" + test_country + "')", function() {
 		it(test_explain, function() {
 			var result = phone(test_number, test_country);
