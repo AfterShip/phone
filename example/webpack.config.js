482789ddf6d61b35ad457b1d0e7c23165db73a2b
SecretBase <whpigpig@gmail.com>
2017-10-10 13:03:41 +0800

    Add browser usage example.

---
+++ example/webpack.config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+	entry: './example/client/index.js',
+	output: {
+		path: path.resolve(__dirname),
+		filename: 'app.js'
+	},
+	devServer: {
+		contentBase: path.join(__dirname),
+		stats: {
+			colors: true,
+			modules: false
+		}
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				loader: 'babel-loader',
+				options: {
+					babelrc: false,
+					presets: [
+						['env', {
+							targets: {
+								browsers: '>1%'
+							},
+							useBuiltIns: true,
+							debug: true,
+							modules: false
+						}]
+					]
+				}
+			}
+		]
+	}
+};
