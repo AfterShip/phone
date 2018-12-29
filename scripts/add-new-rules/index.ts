b1a227754cc52f9c51c95994150b695afc45b265
Jian Gong <j.gong@aftership.com>
2018-12-29 17:24:43 +0800

    fix(lib): some country dosen't have phone_number_length error

--- scripts/add-new-rules/index.ts
+++ scripts/add-new-rules/index.ts
@@ -1,24 +1,48 @@
 require('dotenv').config();
 
-import { IStackDriverError, ITwilioPhoneNumberInstance, Iiso3166 } from './interface';
-import * as _find from 'lodash/find';
-
-const errors: IStackDriverError[] = require('./errors.json');
-const iso3166s: Iiso3166[] = require('../../lib/iso3166Data');
 const accountSid = process.env.TWILIO_SID;
 const authToken = process.env.TWILIO_TOKEN;
+
+~function() {
+    if (!accountSid || !authToken) {
+        throw new Error('Please set environment TWILIO_SID & TWILIO_TOKEN first!');
+    }
+}();
+
+import { IStackDriverError, ITwilioPhoneNumberInstance, Iiso3166 } from './interface';
+import * as _find from 'lodash/find';
+import * as fs from 'fs';
+import * as path from 'path';
+
+const errors: IStackDriverError[] = require('./stackdriver/errors.json');
+const error_phone_array: string[] = require('./stackdriver/error_list.json');
+const iso3166s: Iiso3166[] = require('../../lib/iso3166Data');
 const client = require('twilio')(accountSid, authToken);
 
-const error_numbers = errors.map(({ jsonPayload: {data} }) => {
-                            return client
-                                    .lookups
-                                    .phoneNumbers(data)
-                                    .fetch()
-                                    .catch(err => console.log(err.message));
-                        });
-
 ~async function() {
-    const lookups = await Promise.all(error_numbers);
+    const error_numbers = [...new Set(
+        errors.map(({ jsonPayload: {data} }) => data)
+                .concat(error_phone_array)
+                .map(data => {
+                    const formatted_number = data.replace(/[\s|-]+/g, '');
+                    return formatted_number;
+                }),
+    )];
+    const errorStream = fs.createWriteStream(path.resolve(__dirname, './logs/error.log'));
+    const twilioPromises = error_numbers.map(num => {
+        return client
+                .lookups
+                .phoneNumbers(num)
+                .fetch()
+                .catch(err => {
+                    errorStream.write(`${num}:\n ${JSON.stringify(err, null, 4)}\n`);
+                });
+    });
+
+    const lookups = await Promise.all(twilioPromises).then(v => {
+        errorStream.end();
+        return v;
+    });
     const filteredLookups: ITwilioPhoneNumberInstance[] = lookups.filter(v => v);
     const deduplatedLookups = [...new Set(
         filteredLookups.map(({ phoneNumber, countryCode }) => `${countryCode}:${phoneNumber}`),
@@ -28,44 +52,40 @@ const error_numbers = errors.map(({ jsonPayload: {data} }) => {
     });
 
     const sumByAlpha2 = deduplatedLookups.reduce((sum, { countryCode, phoneNumber }) => {
-        const [{
+        const [matched_item] = iso3166s.filter(({ alpha2 }) => alpha2 === countryCode);
+        const {
             alpha2,
             country_code,
             mobile_begin_with,
             phone_number_lengths,
-        }] = iso3166s.filter(({ alpha2 }) => alpha2 === countryCode);
+        } = matched_item;
         const localPhoneNumber = phoneNumber.replace('+' + country_code, '');
         const isBeginWithRight = mobile_begin_with.length === 0 || mobile_begin_with.some(num => new RegExp(`^${num}`).test(localPhoneNumber));
-        const new_mobile_begin_with = isBeginWithRight ? mobile_begin_with: mobile_begin_with.concat(localPhoneNumber[0]);
+        const new_mobile_begin_with = isBeginWithRight ? mobile_begin_with: mobile_begin_with.concat(localPhoneNumber.slice(0, mobile_begin_with[0].length));
         const new_phone_number_lengths = [...new Set(phone_number_lengths.concat(localPhoneNumber.length))];
         
-        if (sum[alpha2]) {
-            const sum_alpha2 = sum[alpha2];
-            return {
-                ...sum,
-                [alpha2]: {
-                    country_code,
-                    mobile_begin_with: [...new Set([
+        const sum_alpha2 = sum[alpha2];
+        return {
+            ...sum,
+            [alpha2]: {
+                ...matched_item,
+                mobile_begin_with: sum_alpha2 ? 
+                    [...new Set([
                         ...new_mobile_begin_with, 
                         ...sum_alpha2.mobile_begin_with
-                    ])],
-                    phone_number_lengths: [...new Set([
+                    ])] : 
+                    new_mobile_begin_with,
+                phone_number_lengths: sum_alpha2 ?
+                    [...new Set([
                         ...new_phone_number_lengths,
                         ...sum_alpha2.phone_number_lengths,
-                    ])]
-                }
-            };
-        } else {
-            return {
-                ...sum,
-                [alpha2]: {
-                    country_code,
-                    mobile_begin_with: new_mobile_begin_with,
-                    phone_number_lengths: new_phone_number_lengths
-                }
+                    ])] : 
+                    new_phone_number_lengths
             }
-        }
+        };
     }, {});
 
-    console.log( Object.keys(sumByAlpha2).map(alpha2 => ({ alpha2, ...sumByAlpha2[alpha2] })));
+    const resultStream = fs.createWriteStream(path.resolve(__dirname, './logs/result.log'));
+    resultStream.write(JSON.stringify(sumByAlpha2, null, 4));
+    resultStream.end();
 }();
