caf55b7658ca80a9f03a0dc176ba1fdf1975935f
Watson Zuo <hx.zuo@aftership.com>
2020-02-12 14:01:19 +0800

    feat(support node 4): change webpack config to support node 4
    change webpack config to support node 4
    
    TMOC-2750

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const webpack = require('webpack');
 
 module.exports = {
 	// devtool: 'source-map',
-	entry: './lib/index.js',
+	entry: ['babel-polyfill', './lib/index.js'],
 	mode: 'production',
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -33,7 +33,7 @@ module.exports = {
 						['env', {
 							targets: {
 								browsers: '>1%',
-								node: '6.10'
+								node: 4,
 							},
 							modules: false,
 							debug: true
