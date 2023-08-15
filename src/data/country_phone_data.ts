6874d3319bc99f0a768e2ed7b1115f9834cc7845
Vien Klett <58680446+vchau254@users.noreply.github.com>
2023-08-14 21:20:12 -0500

    fix(area-code):add Angullia 2642, 2644 area code (#362)

--- src/data/country_phone_data.ts
+++ src/data/country_phone_data.ts
@@ -60,7 +60,7 @@ export default [
 		alpha3: 'AIA',
 		country_code: '1',
 		country_name: 'Anguilla',
-		mobile_begin_with: ['2645', '2647'],
+		mobile_begin_with: ['2642', '2644', '2645', '2647'],
 		phone_number_lengths: [10]
 	},
 	{
